6de574813a80a3b91eb8290080190c9676bcc7ba
unknown <p2arthur@algogator.finance>
2023-11-05 20:27:28 -0300

    (chore) create breadcrumbs component

---
+++ frontend/src/components/common/breadcrumbs/Breadcrumbs.tsx
@@ -0,0 +1,17 @@
+const Breadcrumbs = ({ pathList }) => {
+  console.log(pathList)
+
+  return (
+    <div className="text-sm breadcrumbs text-gray-300">
+      <ul>
+        {pathList.map((path) => (
+          <li>
+            <p>{path}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default Breadcrumbs
