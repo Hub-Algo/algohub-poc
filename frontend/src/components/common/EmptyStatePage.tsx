b77f10a3715b4bc3740c5629f7e2215aaf5639a5
Arthur Rabelo <arthurrabelopro@gmail.com>
2023-11-13 23:09:31 -0300

    (guard) implement empty state page to block user from starting the process to create a campaign if it's not connected

---
+++ frontend/src/components/common/EmptyStatePage.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const EmptyStatePage = () => {
+  return (
+    <div className="h-screen text-3xl text-gray-400 w-full flex justify-center items-center">
+      Connect your wallet in order to use this functionality
+    </div>
+  )
+}
+
+export default EmptyStatePage
--- frontend/src/pages/campaign-application/CampaignApplication.tsx
+++ frontend/src/pages/campaign-application/CampaignApplication.tsx
@@ -5,16 +5,26 @@ import { useCampaignApplicationStateContext } from './CampaignApplication.contex
 import CampaignApplicationForm from '../../components/campaign/application/view/form/CampaignApplicationForm'
 import CampaignApplicationConfirmationPage from '../../components/campaign/application/view/confirmation/CampaignApplicationConfirmationPage'
 import CampaignApplicationDepositIdoAsa from '../../components/campaign/application/view/deposit-ido-asa/CampaignApplicationDepositIdoAsa'
+import { useWallet } from '@txnlab/use-wallet'
+import EmptyStatePage from '../../components/common/EmptyStatePage'
 
 function CampaignApplication() {
   const { view } = useCampaignApplicationStateContext()
+  const { activeAccount } = useWallet()
 
   return (
-    <PageContainer>
-      <CampaignApplicationSteps />
-
-      {renderView()}
-    </PageContainer>
+    <div>
+      {activeAccount ? (
+        <PageContainer>
+          <CampaignApplicationSteps />
+          {renderView()}
+        </PageContainer>
+      ) : (
+        <PageContainer>
+          <EmptyStatePage />
+        </PageContainer>
+      )}
+    </div>
   )
 
   function renderView() {
